338900c31b0ce8b4f99f13d81cf27dcb12f7c3cf
Kazuki Ota <kaota@microsoft.com>
2024-12-06 11:03:51 +0900

    update

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,16 +1,45 @@
 ﻿using Azure.AI.OpenAI;
 using Azure.Identity;
 using OpenAI.Chat;
+using System.ComponentModel;
+using System.Text.Encodings.Web;
+using System.Text.Json.Nodes;
+using System.Text.Json;
 using System.Text.Json.Schema;
 using System.Text.Json.Serialization;
+using System.Text.Unicode;
 
 var personJsonSchema = JsonSchemaExporter.GetJsonSchemaAsNode(
     SourceGenerationContext.Default.Person,
     exporterOptions: new()
     {
         TreatNullObliviousAsNonNullable = true,
+        // Description を追加する
+        TransformSchemaNode = (context, schema) =>
+        {
+            var attributeProvider = context.PropertyInfo is not null ?
+                context.PropertyInfo.AttributeProvider :
+                context.TypeInfo.Type;
+
+            var description = (DescriptionAttribute?)attributeProvider?.GetCustomAttributes(false)
+                .FirstOrDefault(x => x is DescriptionAttribute);
+
+            if (description == null) return schema;
+
+            if (schema is JsonObject jsonObject)
+            {
+                jsonObject.Insert(0, "description", description.Description);
+            }
+
+            return schema;
+        },
     })
-    .ToJsonString();
+    .ToJsonString(new JsonSerializerOptions
+    {
+        // 見やすいようにインデントと日本語が含まれる場合のエンコードを指定
+        WriteIndented = true,
+        Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
+    });
 
 // AOAI のクライアントを作成する
 var openAiClient = new AzureOpenAIClient(
@@ -51,9 +80,13 @@ Console.WriteLine(result.Value.Content.First().Text);
 [JsonSerializable(typeof(Person))]
 partial class SourceGenerationContext : JsonSerializerContext;
 
+[Description("ユーザー情報")]
 class Person
 {
+    [Description("ユーザーの名前")]
     public string Name { get; set; } = "";
+    [Description("ユーザーの年齢")]
     public int Age { get; set; }
+    [Description("ユーザーの住所")]
     public string[] Addresses { get; set; } = [];
 }
